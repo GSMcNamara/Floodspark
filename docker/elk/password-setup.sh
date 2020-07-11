7b0e00aee2c101fb1e50ac58e014d96c7b54ebad
GS McNamara <floodspark@gmail.com>
2020-07-11 15:06:15 -0400

    logstash_writer role can now write to logstash-* index. needed for geoip data type template transformation

--- docker/elk/password-setup.sh
+++ docker/elk/password-setup.sh
@@ -2,5 +2,5 @@
 
 sh -c "/password-setup.exp"
 
-curl --user elastic:qE*7Pj43Or -H "Content-Type: application/json" -d '{"cluster": ["manage_index_templates", "monitor"], "indices": [{"names": [ "*" ], "privileges": ["write","create","delete","create_index"]}]}' -X POST http://localhost:9200/_xpack/security/role/logstash_writer
+curl --user elastic:qE*7Pj43Or -H "Content-Type: application/json" -d '{"cluster": ["manage_index_templates", "monitor"], "indices": [{"names": [ "*","logstash-*" ], "privileges": ["write","create","delete","create_index"]}]}' -X POST http://localhost:9200/_xpack/security/role/logstash_writer
 curl --user elastic:qE*7Pj43Or -H "Content-Type: application/json" -d '{"password" : "qE*7Pj43Or","roles" : [ "logstash_writer"],"full_name" : "Internal Logstash User"}' -X POST http://localhost:9200/_xpack/security/user/logstash_internal
